c21dd2267f603f01ba4118756e702baf40a4a83a
Swathi9225 <tadiboinaswathi@gmail.com>
2026-01-13 15:03:23 +0530

    created a javascript file

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+/*console.log("Hello, Node.js!");
+// basic operatores in java script//
+console.log("adding"+ " two"+ " numbers");
+console.log(123+321);
+console.log("difference"+ " between"+ " two"+ " numbers");
+console.log(987-789);
+console.log("product"+ " of"+" two"+" numbers");
+console.log(234*12);
+console.log("division"+ " of"+ " two"+ " numbers")
+console.log(250/5);
+console.log("remainder"+ " of"+ " two"+ " numbers")
+console.log(150%10);*/
+
+console.log(varibales);
+var a=10;
+var b=20;
+var c=suma+b;
+console.log();
