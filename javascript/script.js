eb547f4afef059c8773418011312497c1d6f2857
Swathi9225 <tadiboinaswathi@gmail.com>
2026-01-22 16:28:52 +0530

    js

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,70 @@
-/*console.log("Hello, Node.js!");
-// basic operatores in java script//
+console.log("Hello, Node.js!");
+// basic operatores in java script
 console.log("adding"+ " two"+ " numbers");
 console.log(123+321);
 console.log("difference"+ " between"+ " two"+ " numbers");
 console.log(987-789);
 console.log("product"+ " of"+" two"+" numbers");
 console.log(234*12);
-console.log("division"+ " of"+ " two"+ " numbers")
+console.log("division"+ " of"+ " two"+ " numbers");
 console.log(250/5);
-console.log("remainder"+ " of"+ " two"+ " numbers")
-console.log(150%10);*/
+console.log("remainder"+ " of"+ " two"+ " numbers"); 
+console.log(150%10);
 
-console.log(varibales);
-var a=10;
-var b=20;
-var c=suma+b;
-console.log();
+
+
+    //BMI Calculation
+    let weight = 48
+    let height = 5.11
+    let BMI = (weight/height*2)
+        console.log(`The BMI is :${BMI}`);
+
+    //KMs TO Miles
+    let km =150
+    const one_mile = 0.62137119
+    let mile = (km*one_mile)
+        console.log(`150 km equals to ${mile} miles`);
+
+
+// control statments
+
+let legal_age = 18
+ let user_age =20
+ let  isage = user_age >= legal_age
+    if(isage){
+        console.log("you can vote");
+    }else{
+        console.log("you can't vote");
+        
+    }
+     
+    
+    let UserName = prompt("Enter You're UserName");
+        if(UserName==="durga"){
+            console.log(`Welcome ${UserName}`);
+        }
+    let PassWord = prompt("Enter You're PassWord");
+            if(PassWord==="Swathi2005" && PassWord.length12){
+                console.log("PassWord saved");
+             }
+             else{
+                console.log("Invalid PassWord , length missed");
+                }
+    let age = prompt("Enter your legal age :");
+        if(age>=18) {
+            console.log(":");
+         }
+           else if(!UserName || !PassWord) {
+                console.log("UserName and PassWord connot be empty"); 
+            }
+           
+             else {
+                console.log("welcome to Account");
+                }
+    
+
+
+
+
+                
+ 
