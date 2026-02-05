73bedee289dad112835068c63bbe38f638fc1eb0
Swathi9225 <tadiboinaswathi@gmail.com>
2026-02-05 15:55:37 +0530

    done changes

--- javascript/form.js
+++ javascript/form.js
@@ -1,6 +1,6 @@
 const UserName = document.querySelector('#Username')
 UserName.addEventListener(
-    'mouseenter',()=>{
+    'click',()=>{
         console.log("Enter your UserName")
         if(UserName.value ==="Swathi"){
         console.log("Hello!."); 
@@ -11,36 +11,43 @@ UserName.addEventListener(
    
 const Email = document.querySelector('#Email')
 Email.addEventListener(
-    'mouseenter',()=>{
+    'click',()=>{
         console.log("Enter your Email")
         if(Email.value ==="Swathi@gmail.com"){
         console.log("Email Verified!"); 
     }
     },false
-);
-
-   
+)
 
 
-
-
-const PhoneNO = document.querySelector('#phone_no')
- PhoneNO.addEventListener(
-    'mouseenter',()=>{
-        console.log("Enter your Phone No")
-        if(PhoneNO.value.length === 10){
-        console.log("PhoneNo Saved!"); 
-    }
-    },false
-);
 
 const Password = document.querySelector('#Password')
 Password.addEventListener(
-    'mouseenter',()=>{
+    'click',()=>{
         console.log("Enter your Valied Password")
         if(Password.value ==="Swathi123"){
         console.log("Welcom!!."); 
     }
     },false
-);
+)
+
+
+ const button_element = document.querySelector('#btn')
+
+button_element.addEventListener(
+         'click',  () => {
+        console.log("Uploded!")
+        
+    },
+     false
+ )
+ click_button.addEventListener('click', () => {
+    if (toggle) {
+     h2_element.id = 'color'
+      toggle = false
+    } else {
+        h2_element.id = ''
+        toggle = true
+    }
+})
 
