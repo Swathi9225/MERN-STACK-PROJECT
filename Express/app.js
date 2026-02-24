2efe2fd1d688eedf13b1d15f7a96704d2d76f2a8
Swathi9225 <tadiboinaswathi@gmail.com>
2026-02-24 16:20:41 +0530

    changes

--- Express/app.js
+++ Express/app.js
@@ -6,6 +6,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/sir', (req, res) => {
+  res.send("<h1>Hello sir Please don't scary me!!</h1>")
+})
 
 app.get('/videos', (req, res) => {
   res.send('video page!')
